c0ef2ffc3ce875202fd1259a48a41eb003ceb7f0
blezzydcruz <mbannedelacruz@hotmail.com>
2023-07-06 15:59:14 +0100

    fourth commit

---
+++ extension/src/WeatherCheck.java
@@ -0,0 +1,17 @@
+public class WeatherCheck {
+
+        public static void main(String[] args) {
+            int temperature = 22; //current temperature
+            boolean currentlyRaining = false; //Is it raining?
+
+            if(temperature >= 20 && currentlyRaining) {
+                System.out.println("It's hot and raining! Wear shorts and bring an umbrella");
+            } else if(temperature >= 20 && !currentlyRaining) {
+                System.out.println("It's hot and not raining! Wear shorts and sunscreen ");
+            } else if(temperature < 20 && currentlyRaining) {
+                System.out.println("It's cold and raining! Wear a waterproof coat");
+            } else {
+                System.out.println("It's cold and not raining! Wear a coat");
+            }
+        }
+}
